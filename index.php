678cb01d8e35c84e5d44a567710667cbf72eb377
faizalbahasan <faizalbahasan2010@gmail.com>
2021-01-22 12:46:58 +0800

    Kineticpay
    Kineticpay For Wordpress

---
+++ index.php
@@ -0,0 +1,164 @@
+<?php
+/**
+ * Plugin Name: kineticPay for WooCommerce
+ * Plugin URI: https://kineticpay.my/
+ * Description: Receive payment on your WooCommerce site via kineticPay.
+ * Version: 1.0.0
+ * Author: kineticPay
+ * Author URI: https://kineticpay.my/
+ * WC requires at least: 2.6.0
+ * WC tested up to: 5.5.1
+ **/
+
+// No direct access
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+// Init kineticPay
+add_action( 'plugins_loaded', 'kineticpay_init', 0 );
+function kineticpay_init() {
+	if ( ! class_exists( 'WC_Payment_Gateway' ) ) {
+		return;
+	}
+
+	include_once( 'src/kineticpay.php' );
+
+	add_filter( 'woocommerce_payment_gateways', 'add_kineticpay_to_woocommerce' );
+	function add_kineticpay_to_woocommerce( $methods ) {
+		$methods[] = 'kineticPay';
+
+		return $methods;
+	}
+}
+
+// Add setting link to plugin list
+add_filter( 'plugin_action_links_' . plugin_basename( __FILE__ ), 'kineticpay_links' );
+function kineticpay_links( $links ) {
+	$plugin_links = array(
+		'<a href="' . admin_url( 'admin.php?page=wc-settings&tab=checkout&section=kineticpay' ) . '">' . __( 'Settings', 'kineticpay' ) . '</a>',
+	);
+
+	return array_merge( $plugin_links, $links );
+}
+
+// Check init response
+add_action( 'init', 'kineticpay_check_response', 15 );
+function kineticpay_check_response() {
+	if ( ! class_exists( 'WC_Payment_Gateway' ) ) {
+		return;
+	}
+
+	include_once( 'src/kineticpay.php' );
+
+	$kineticpay = new kineticpay();
+	$kineticpay->check_kineticpay_callback();
+}
+
+// Add bank list to Checkout page
+add_filter( 'woocommerce_gateway_description', 'kineticpay_bank_list', 20, 2 );
+function kineticpay_bank_list( $description, $payment_id ){
+    if( 'kineticPay' === $payment_id ){
+        ob_start();
+        $bank_name = array(
+            '' => 'Select Bank',
+            'ABMB0212' => 'Alliance Bank Malaysia Berhad',
+            'ABB0233' => 'Affin Bank Berhad',
+            //'ABB0234' => 'Affin Bank Berhad',
+            'AMBB0209' => 'AmBank (M) Berhad',
+            //'BPMBMYKL' => 'AGROBANK',
+            'BCBB0235' => 'CIMB Bank Berhad',
+            'BIMB0340' => 'Bank Islam Malaysia Berhad',
+            'BKRM0602' => 'Bank Kerjasama Rakyat Malaysia Berhad',
+            'BMMB0341' => 'Bank Muamalat (Malaysia) Berhad',
+            'BSN0601' => 'Bank Simpanan Nasional Berhad',
+            'CIT0219' => 'Citibank Berhad',
+            'HLB0224' => 'Hong Leong Bank Berhad',
+            //'HBMBMYKL' => 'HSBC Bank Malaysia Berhad',
+            'HSBC0223' => 'HSBC Bank Malaysia Berhad',
+            'KFH0346' => 'Kuwait Finance House',
+            'MB2U0227' => 'Maybank2u / Malayan Banking Berhad',
+            //'MBBEMYKL' => 'Maybank2u / Malayan Banking Berhad',
+            //'MBB0227' => 'Maybank2E / Malayan Banking Berhad E',
+            'MBB0228' => 'Maybank2E / Malayan Banking Berhad E',
+            'OCBC0229' => 'OCBC Bank (Malaysia) Berhad',
+            'PBB0233' => 'Public Bank Berhad',
+            //'RJHIMYKL' => 'AL RAJHI BANKING & INVESTMENT CORPORATION (MALAYSIA) BERHAD',
+            //'RHBBMYKL' => 'RHB Bank Berhad',
+            'RHB0218' => 'RHB Bank Berhad',
+            'SCB0216' => 'Standard Chartered Bank (Malaysia) Berhad',
+            'UOB0226' => 'United Overseas Bank (Malaysia) Berhad',
+            //'UOB0229' => 'United Overseas Bank (Malaysia) Berhad',
+        );
+
+        echo '<div class="kineticpay-bank" style="padding:10px 0;">';
+        woocommerce_form_field( 'kineticpay_bank', array(
+            'type'          => 'select',
+            'label'         => __("Choose Payment Method", "woocommerce"),
+            'class'         => array('form-row-wide'),
+            'required'      => true,
+            'options'       => $bank_name,
+            'default'       => '',
+        ), '');
+        echo '<div>';
+        $description .= ob_get_clean();
+    }
+    return $description;
+}
+
+// Check if bank is selected
+add_action('woocommerce_checkout_process', 'kineticpay_check_bank' );
+function kineticpay_check_bank() {
+    if ( isset($_POST['payment_method']) && $_POST['payment_method'] === 'kineticPay'
+    && isset($_POST['kineticpay_bank']) && empty($_POST['kineticpay_bank']) ) {
+        wc_add_notice( __( 'Please select bank name for payment, please.' ), 'error' );
+    }
+}
+
+// Add bank code to order
+add_action('woocommerce_checkout_create_order', 'kineticpay_write_to_meta_data', 10, 2 );
+function kineticpay_write_to_meta_data( $order, $data ) {
+    if ( isset($_POST['kineticpay_bank']) && ! empty($_POST['kineticpay_bank']) ) {
+        $order->update_meta_data( '_kineticpay_bank' , esc_attr($_POST['kineticpay_bank']) );
+    }
+}
+
+// Help to pass error on Checkout or Thank You page
+add_action('woocommerce_before_checkout_form','kineticpay_err_param');
+add_action('woocommerce_thankyou','kineticpay_err_param');
+function kineticpay_err_param() { 
+    if (isset($_REQUEST['kp_notification'])) {
+        wc_print_notice($_REQUEST['kp_msg'], $_REQUEST['kp_type']);
+    }
+}
+
+// Add requery option to edit Order
+add_action( 'woocommerce_order_actions', 'kineticpay_order_requery' );
+function kineticpay_order_requery( $actions ) {
+    global $theorder;
+
+    if ( $theorder->is_paid() ) {
+        return $actions;
+    }
+
+    $actions['kineticpay_do_query'] = __( 'Requery payment status from kineticPay', 'kineticPay' );
+    return $actions;
+}
+
+// Process requery from kineticPay server
+add_action( 'woocommerce_order_action_kineticpay_do_query', 'kineticpay_requery_process' );
+function kineticpay_requery_process( $customer_order ) {
+	$order_id = $customer_order->get_id();
+    $getstatus = wp_remote_get('https://manage.kineticpay.my/payment/status?merchant_key='. get_option('woocommerce_kineticPay_settings')['merchant_key'] .'&invoice=' . $order_id);
+	$result = json_decode($getstatus["body"], true);
+	
+    if (array_key_exists('code', $result) && $result['code'] == "00"){
+		$customer_order->add_order_note('Payment via kineticPay was succeed.<br>Transaction ID: ' . $result['id']);
+		$customer_order->payment_complete();
+	} else 
+	if (array_key_exists('code', $result)){
+		$customer_order->add_order_note('Payment via kineticPay was failed.<br>Error code: ' . $result['code'] . '<br>Transaction ID: ' . $result['id']);
+	} else {
+		$customer_order->add_order_note('Payment via kineticPay was failed without code.');
+	}
+}
---
+++ src/kineticpay.php
@@ -0,0 +1,180 @@
+<?php
+/**
+
+ * kineticPay Payment Gateway Classs
+
+ */
+
+class kineticpay extends WC_Payment_Gateway {
+
+	function __construct(){
+
+		add_action( 'woocommerce_api_callback', 'check_kineticpay_callback' );
+
+		$this->id = "kineticPay";
+		$this->method_title = __("kineticPay", 'kineticPay');
+		$this->method_description = __("Enable your customers to make payments securely via kineticPay.", 'kineticPay');
+		$this->title = __("kineticPay", 'kineticPay');
+		$this->icon = plugins_url('assets/kineticpay-logo-all.png', __FILE__);
+		$this->has_fields = true;
+		$this->init_form_fields();
+		$this->init_settings();
+
+		foreach ($this->settings as $setting_key => $value) {
+			$this->$setting_key = $value;
+		}
+
+		if (is_admin()) {
+			add_action('woocommerce_update_options_payment_gateways_' . $this->id, array(
+				$this,
+				'process_admin_options'
+			));
+		}
+	}
+
+	public function init_form_fields()
+
+	{
+
+		$this->form_fields = array(
+			'kp_enabled'        => array(
+				'title'   => __('Enable / Disable', 'kineticPay'),
+				'label'   => __('Enable this payment gateway', 'kineticPay'),
+				'type'    => 'checkbox',
+				'default' => 'no',
+			),
+
+			'title'          => array(
+				'title'    => __('Title', 'kineticPay'),
+				'type'     => 'text',
+				'default'  => __('kineticPay', 'kineticPay'),
+			),
+
+			'description'    => array(
+				'title'    => __('Description', 'kineticPay'),
+				'type'     => 'textarea',
+				'default'  => __('Pay securely with kineticPay.', 'kineticPay'),
+				'css'      => 'max-width:350px;',
+			),
+
+			'merchant_key'      => array(
+				'title'    => __('Merchant Key', 'kineticPay'),
+				'type'     => 'text',
+				'desc_tip' => __('Required', 'kineticPay'),
+				'description' => __('Obtain your merchant key from your kineticPay dashboard.', 'kineticPay'),
+			),
+			// Future if category implemented
+			/*'category_key' => array(
+				'title'    => __('Category Code', 'kineticPay'),
+				'type'     => 'text',
+				'desc_tip' => __('Required', 'kineticPay'),
+				'description' => __('Create a category at your kineticPay dashboard and fill in your category code here.', 'kineticPay'),
+			),*/
+		);
+	}
+
+	public function process_payment($order_id)
+	
+	{
+		do_action( 'woocommerce_loaded' );
+		
+		$customer_order = wc_get_order($order_id);
+		$callbackURL = add_query_arg(array('wc-api' => 'kineticpay', 'order' => $order_id, 'process' => 'processing'), home_url('/'));
+		$name = $customer_order->get_billing_first_name() . ' ' . $customer_order->get_billing_last_name();
+		$email = $customer_order->get_billing_email();
+		$phone = $customer_order->get_billing_phone();
+		$bankid = $customer_order->get_meta('_kineticpay_bank');
+
+		if($name == NULL || $phone == NULL || $email == NULL){
+			wc_add_notice( 'Error! Please complete your details (Name, phone, and e-mail are compulsory).', 'error' );
+			return;
+		}
+
+		return array(
+			'result'    => 'success',
+			'redirect'  => $callbackURL
+		);
+	}
+	
+	public function check_kineticpay_callback()
+	
+	{
+	    do_action( 'woocommerce_loaded' );
+	    
+	    if (isset($_REQUEST['order'])){
+    	    $order_id = $_REQUEST['order'];
+    	    $customer_order = wc_get_order($order_id);
+    	    
+    	    if ($customer_order && $order_id != 0 && isset($_REQUEST['process'])){
+    	        $bankid     = $customer_order->get_meta('_kineticpay_bank');
+    	        if ($_REQUEST['process'] == 'processing'){
+                    $amount = $customer_order->get_total();
+    	            $redirectURL = add_query_arg(array('wc-api' => 'kineticpay'), home_url('/'));
+                    $secretkey = $this->merchant_key;
+                    $categorycode = $this->category_key;
+					$name = $customer_order->get_billing_first_name() . ' ' . $customer_order->get_billing_last_name();
+					$email = $customer_order->get_billing_email();
+					$phone = $customer_order->get_billing_phone();
+					$description = "Payment for Order No " .  $order_id . ", Buyer Name " . $email . ", Email " . $phone . ", Phone No. " . $phone;
+                    
+                    $data = [
+            			'callback_success'  => $redirectURL,
+            			'callback_error'    => $redirectURL,
+            			'amount'            => $amount,
+            			'description'       => $description,
+            			'invoice'           => $order_id,
+            			'merchant_key'      => $secretkey,
+            			'bank'              => $bankid,
+            		];
+            		
+                    $getstatus = wp_remote_get('https://manage.kineticpay.my/payment/create',array('body' => $data, 'method' => 'POST'));
+    	            $result = json_decode($getstatus["body"], true);
+    	            if (array_key_exists('html', $result)){
+    	                $customer_order->add_order_note('Customer made a payment attempt using bank ID '. $bankid .' via kineticPay.');
+            		    echo $result["html"];
+            		} else {
+            		    wc_add_notice('Payment was declined. Something error with payment gateway, please contact store manager.', 'error');
+						//var_dump($result);
+            		    wp_redirect(wc_get_checkout_url());
+            		}
+            		exit();
+    	        }
+    	    }
+	    } else 
+	    if (isset($_REQUEST['fpx_sellerOrderNo'])){
+	        $fpxdata = explode("-", $_REQUEST['fpx_sellerOrderNo']);
+	        $order_id = $fpxdata[1];
+    	    $customer_order = wc_get_order($order_id);
+    	    
+    	    $getstatus = wp_remote_get('https://manage.kineticpay.my/payment/status?merchant_key='.$this->merchant_key.'&invoice=' . $order_id);
+    	    $result = json_decode($getstatus["body"], true);
+    	    
+            if (array_key_exists('code', $result) && $result['code'] == "00"){
+                $customer_order->add_order_note('Payment via kineticPay was succeed.<br>Transaction ID: ' . $result['id']);
+                $customer_order->payment_complete();
+                wp_safe_redirect(add_query_arg( array(
+                    'kp_notification' => 1,
+                    'kp_msg' => 'Payment Successful. Your payment has been successfully completed.',
+                    'kp_type' => 'success'),
+                $customer_order->get_checkout_order_received_url()));
+            } else 
+            if (array_key_exists('code', $result)){
+                $customer_order->add_order_note('Payment via kineticPay was failed.<br>Error code: ' . $result['code'] . '<br>Transaction ID: ' . $result['id']);
+                wp_safe_redirect(add_query_arg( array(
+                    'kp_notification' => 1,
+                    'kp_msg' => 'Sorry, your payment failed. No charges were made.',
+                    'kp_type' => 'error'),
+                wc_get_checkout_url()));
+            } else {
+                $customer_order->add_order_note('Payment via kineticPay was failed without code.');
+                wp_safe_redirect(add_query_arg( array(
+                    'kp_notification' => 1,
+                    'kp_msg' => 'Sorry, your payment failed. No charges were made.',
+                    'kp_type' => 'error'),
+                wc_get_checkout_url()));
+            }
+            
+            exit();
+        }
+	}
+}
